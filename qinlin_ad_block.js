2b4b5ab20f85ef9a6092ab16612aeba0bc206204
pascal878 <62474676+pascal878@users.noreply.github.com>
2025-07-27 21:37:23 +0800

    Update qinlin_ad_block.js

--- qinlin_ad_block.js
+++ qinlin_ad_block.js
@@ -1,15 +1,22 @@
-// qinlin_ad_block.js
 let body = {};
 try {
-    body = JSON.parse($response.body);
-} catch(e) {
-    body = {};
+  body = JSON.parse($response.body);
+} catch (e) {
+  body = {};
 }
+
+// 通用清空策略
+function deepClean(obj) {
+  if (Array.isArray(obj)) return [];
+  if (typeof obj === 'object') {
+    for (let key in obj) obj[key] = (typeof obj[key] === 'object') ? deepClean(obj[key]) : null;
+    return obj;
+  }
+  return {};
+}
+
 if (body.data) {
-    if (Array.isArray(body.data)) {
-        body.data = [];
-    } else if (typeof body.data === 'object') {
-        for (let key in body.data) delete body.data[key];
-    }
+  body.data = deepClean(body.data);
 }
+
 $done({ body: JSON.stringify(body) });
