cdc290774e3cee394251c5d45744fc35338f3051
pascal878 <62474676+pascal878@users.noreply.github.com>
2025-07-27 22:09:12 +0800

    Update qinlin_ad_block.js

--- qinlin_ad_block.js
+++ qinlin_ad_block.js
@@ -1,22 +1,4 @@
-let body = {};
-try {
-  body = JSON.parse($response.body);
-} catch (e) {
-  body = {};
-}
-
-// 通用清空策略
-function deepClean(obj) {
-  if (Array.isArray(obj)) return [];
-  if (typeof obj === 'object') {
-    for (let key in obj) obj[key] = (typeof obj[key] === 'object') ? deepClean(obj[key]) : null;
-    return obj;
-  }
-  return {};
-}
-
-if (body.data) {
-  body.data = deepClean(body.data);
-}
-
-$done({ body: JSON.stringify(body) });
+// adblock_qinlin.js
+let obj = JSON.parse($response.body);
+obj = {}; // 返回空广告数据
+$done({body: JSON.stringify(obj)});
